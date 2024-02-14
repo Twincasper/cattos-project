7c021a28ae65e669f9ffb7ed6904efb4d9d24a23
Twincasper <justinwatson651@gmail.com>
2024-02-14 14:30:55 -0500

    updated build

--- robohash-app/src/components/Card.jsx
+++ robohash-app/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-blue br3 pa3 ma2 dib bw2 shadow-5'>
+    <div className='tc grow bg-light-blue br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='cats' src={`https://robohash.org/${id}?set=set4&size=200x200`} />
       <div>
         <h2>{name}</h2>
