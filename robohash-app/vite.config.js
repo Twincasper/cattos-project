9e2a5da040b2f8c7a63bc40ecb0dc0386b8d19c7
Twincasper <justinwatson651@gmail.com>
2024-03-12 18:05:30 -0400

    Updated build

--- robohash-app/vite.config.js
+++ robohash-app/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: { outDir: '..' },
-  base: '/cattos-project/'
+  base: '/cattos-project/',
 })
